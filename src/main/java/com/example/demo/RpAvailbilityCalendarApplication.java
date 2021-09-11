9f198765ea1cd92ea5f5ae240fd9050791be8f33
Remy Ward <remyward@protonmail.com>
2021-09-11 13:44:54 -0500

    initial commit

---
+++ src/main/java/com/example/demo/RpAvailbilityCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpAvailbilityCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpAvailbilityCalendarApplication.class, args);
+	}
+
+}
